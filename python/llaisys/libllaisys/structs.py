79274e3795f4c79ead26bb8cdb52161e6cac5733
binliu <87918526+llaboon@users.noreply.github.com>
2026-02-05 15:05:04 +0800

    Update structs.py

--- python/llaisys/libllaisys/structs.py
+++ python/llaisys/libllaisys/structs.py
@@ -1,10 +1,7 @@
-# python/llaisys/libllaisys/models.py
-
 import ctypes
-from . import LIB_LLAISYS
 from .tensor import llaisysTensor_t
 
-# C struct definitions matching include/llaisys/models/qwen2.h
+# Definitions matching include/llaisys/models/qwen2.h
 
 class LlaisysQwen2Meta(ctypes.Structure):
     _fields_ = [
@@ -27,6 +24,7 @@ class LlaisysQwen2Weights(ctypes.Structure):
         ("in_embed", llaisysTensor_t),
         ("out_embed", llaisysTensor_t),
         ("out_norm_w", llaisysTensor_t),
+        # Pointers to arrays of tensors
         ("attn_norm_w", ctypes.POINTER(llaisysTensor_t)),
         ("attn_q_w", ctypes.POINTER(llaisysTensor_t)),
         ("attn_q_b", ctypes.POINTER(llaisysTensor_t)),
@@ -40,25 +38,3 @@ class LlaisysQwen2Weights(ctypes.Structure):
         ("mlp_up_w", ctypes.POINTER(llaisysTensor_t)),
         ("mlp_down_w", ctypes.POINTER(llaisysTensor_t)),
     ]
-
-# Function Prototypes
-LIB_LLAISYS.llaisysQwen2ModelCreate.argtypes = [
-    ctypes.POINTER(LlaisysQwen2Meta),
-    ctypes.c_int,           # llaisysDeviceType_t
-    ctypes.POINTER(ctypes.c_int), # device_ids
-    ctypes.c_int            # ndevice
-]
-LIB_LLAISYS.llaisysQwen2ModelCreate.restype = ctypes.c_void_p
-
-LIB_LLAISYS.llaisysQwen2ModelDestroy.argtypes = [ctypes.c_void_p]
-LIB_LLAISYS.llaisysQwen2ModelDestroy.restype = None
-
-LIB_LLAISYS.llaisysQwen2ModelWeights.argtypes = [ctypes.c_void_p]
-LIB_LLAISYS.llaisysQwen2ModelWeights.restype = ctypes.POINTER(LlaisysQwen2Weights)
-
-LIB_LLAISYS.llaisysQwen2ModelInfer.argtypes = [
-    ctypes.c_void_p,
-    ctypes.POINTER(ctypes.c_int64), # token_ids
-    ctypes.c_size_t                 # ntoken
-]
-LIB_LLAISYS.llaisysQwen2ModelInfer.restype = ctypes.c_int64
